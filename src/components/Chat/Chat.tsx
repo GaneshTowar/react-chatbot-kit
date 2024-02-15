6cd5a5e2b79f8465176fae3b82e78778f0c6ce1c
GaneshTowar <47410765+GaneshTowar@users.noreply.github.com>
2024-02-15 17:22:47 +0530

    mike margin fix

--- src/components/Chat/Chat.tsx
+++ src/components/Chat/Chat.tsx
@@ -350,9 +350,9 @@ const Chat = ({
               style={customButtonStyle}
             >   
                 {listening ? (
-                  <MdKeyboardVoice style={{ color: 'green', fontSize: 24 }} />
+                  <MdKeyboardVoice style={{ color: 'green', fontSize: 24, marginTop: 3 }} />
                 ) : (
-                  <MdKeyboardVoice style={{ color: 'red', fontSize: 24 }} />
+                  <MdKeyboardVoice style={{ color: 'red', fontSize: 24, marginTop: 3 }} />
                 )}
             </div>
 
